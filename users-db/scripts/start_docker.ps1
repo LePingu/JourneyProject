1e038f4f6e3badadb8b63f592538f95c260a369d
LePingu <hatim.nourbay@gmail.com>
2020-06-02 11:55:32 +0100

    Added docker compose logic to set up local project

--- users-db/scripts/start_docker.ps1
+++ users-db/scripts/start_docker.ps1
@@ -8,8 +8,9 @@ Write-Host "Build finished"
 Write-Host "Image id is:" 
 docker inspect --format="{{.Id}}" journey-project/users-db
 
-Start-Process
-
-docker run --rm -P --name pg_test journey-project/users-db
+start powershell {
+    $Host.UI.RawUI.WindowTitle='Users PostGres'
+    docker run -p 32776:5432 --name users_db journey-project/users-db
+}
 
 Pop-Location
